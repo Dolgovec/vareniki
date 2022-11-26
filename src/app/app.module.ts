f48a06e07bee6170045d40c5c79fb87e8b6de3bc
dmytroponomarenko <d.ponomarenko@comparus.de>
2022-11-26 12:19:49 +0200

    css to scss. Added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HomeModule} from "./pages/home/home.module";
+import {EventsModule} from "./pages/events/events.module";
+import {Route, RouterModule} from "@angular/router";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+
+const routes: Route[] = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'events',
+    loadChildren: () => import('./pages/events/events.module').then(m => m.EventsModule)
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule,
+    EventsModule,
+    RouterModule.forRoot(routes),
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/events/event-page/event-page.component.ts
+++ src/app/pages/events/event-page/event-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-event-page',
   templateUrl: './event-page.component.html',
-  styleUrls: ['./event-page.component.css']
+  styleUrls: ['./event-page.component.scss']
 })
 export class EventPageComponent implements OnInit {
 
